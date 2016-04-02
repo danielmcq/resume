7ea12d8115467ec749d0807a4a6aa142e7c3d0af
Daniel McQuiston <daniel.pro@dehugo.net>
2016-04-02 11:21:53 -0600

    Format code

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
+/*eslint no-console:0*/
 "use strict"
 
-const express  = require('express')
-     ,app      = express()
-     ,template = require('jade').compileFile(__dirname + "/src/templates/main.jade")
-     ,fs       = require("fs")
-     ,utils    = require(__dirname+"/src/js/Utils")
+const express    = require("express")
+const app        = express()
+const template   = require("jade").compileFile(__dirname + "/src/templates/main.jade")
+const fs         = require("fs")
+const utils      = require(__dirname+"/src/js/Utils")
+const dateformat = require("dateformat")
+const phone      = require("phone-formatter")
 
-app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname + "/static"))
 
-app.get('/', function (req, res, next) {
-  try {
-    fs.readFile(__dirname+"/data.json", (err,contents)=>{
-      let locals = JSON.parse(contents, utils.JSON.dateParser)
-      let html = template(locals)
+app.get("/", function (req, res, next) {
+	try {
+		fs.readFile(__dirname+"/data.json", (err,contents)=>{
+			let locals = Object.assign(
+				{
+					"$utils": {
+						dateformat: dateformat,
+						shortDate: (date)=> dateformat.call(dateformat, date, "mmm yyyy"),
+						dateRange: (start, end) => locals.$utils.shortDate(start)+" - "+(end?locals.$utils.shortDate(end):"Present"),
+						phone: phone
+					}
+				},
+				JSON.parse(contents, utils.JSON.dateParser)
+			)
 
-      res.send(html)
-    })
-  } catch (e) {
-    next(e)
-  }
+			let html = template(locals)
+
+			res.send(html)
+		})
+	} catch (e) {
+		next(e)
+	}
 })
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+	console.log("Listening on http://localhost:" + (process.env.PORT || 3000))
 })
